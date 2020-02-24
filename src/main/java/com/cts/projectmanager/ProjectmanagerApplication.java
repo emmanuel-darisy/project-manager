483b4b42fd5425e57963dd6a2a35eca0cf65f5c1
emmanuel-darisy <emmanuel5978@gmail.com>
2020-02-24 22:17:44 +0530

    Code changes to generate .war file as artifact

--- src/main/java/com/cts/projectmanager/ProjectmanagerApplication.java
+++ src/main/java/com/cts/projectmanager/ProjectmanagerApplication.java
@@ -1,21 +1,26 @@
 package com.cts.projectmanager;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
 @SpringBootApplication
-public class ProjectmanagerApplication {
+public class ProjectmanagerApplication extends SpringBootServletInitializer{
 //private static Logger logger = LoggerFactory.getLogger(ProjectmanagerApplication.class);
 	
 	public static void main(String[] args) {
 		log.debug("PMA2");
 		SpringApplication.run(ProjectmanagerApplication.class, args);
+	}
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProjectmanagerApplication.class);
+		
 	}
 
 }
